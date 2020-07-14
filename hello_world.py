701918b850a44c323362b8a47b2018ba37851ff7
zl7910 <zl_791010@163.com>
2020-07-14 15:21:30 +0800

    nothing2

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
 print("Hello Git world!")
 print("Hello everyone.")
 print("Hello nothing.")
+print("11111.")
--- hello_world2.py
+++ hello_world2.py
@@ -1,2 +1,3 @@
 print("Hello Git world2!")
 print("Hello everyone2.")
+print("aaaaaaaaaaaa.")
